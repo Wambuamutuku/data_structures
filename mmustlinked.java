8a455adba0e47b3c8c62db3844fa1452afb7f167
Wambuamutuku <104725632+Wambuamutuku@users.noreply.github.com>
2023-03-09 16:28:44 +0300

    Create mmustlinked.java

---
+++ mmustlinked.java
@@ -0,0 +1,65 @@
+/*author
+ wambua mutuku- com/b/01-00140/2021
+james wainaina- com/b/o1-00135/2021
+dennis muuo-c0m/b01-
+charles 
+/*
+
+
+public class Mmustlinkedin {
+
+    private class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+
+    public Mmustlinkedin() {
+        this.head = null;
+    }
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    public void remove(int data) {
+        if (head == null) {
+            return;
+        }
+        if (head.data == data) {
+            head = head.next;
+            return;
+        }
+        Node current = head;
+        while (current.next != null && current.next.data != data) {
+            current = current.next;
+        }
+        if (current.next != null) {
+            current.next = current.next.next;
+        }
+    }
+
+    public void print() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
